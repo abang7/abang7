8ba8e4e56440b1244ca18847b8f8840d7beffd33
bang <947928577@qq.com>
2024-06-18 05:31:06 +0000

    v1

---
+++ src/main/java/com/example/spteest/SpteestApplication.java
@@ -0,0 +1,13 @@
+package com.example.spteest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpteestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpteestApplication.class, args);
+    }
+
+}
